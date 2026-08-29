a7982524f9e019436a28a3f00f005dcda35008af
WattIQs <amaorbelo@gmail.com>
2026-08-29 10:28:57 -0300

    Add TypeScript declaration for Lottie web component

---
+++ src/lottie-elements.d.ts
@@ -0,0 +1,11 @@
+declare namespace JSX {
+  interface IntrinsicElements {
+    "dotlottie-wc": React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement> & {
+      src?: string;
+      autoplay?: boolean;
+      loop?: boolean;
+      mode?: string;
+      speed?: string | number;
+    };
+  }
+}
