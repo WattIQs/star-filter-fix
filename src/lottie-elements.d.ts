d04df533954e436dde85fd5d602458c977902157
WattIQs <amaorbelo@gmail.com>
2026-08-29 10:33:26 -0300

    fix Lottie React 19 JSX declaration

--- src/lottie-elements.d.ts
+++ src/lottie-elements.d.ts
@@ -1,6 +1,6 @@
 import type * as React from "react";
 
-declare global {
+declare module "react" {
   namespace JSX {
     interface IntrinsicElements {
       "dotlottie-wc": React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement> & {
