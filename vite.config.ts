ebe1fd4b4aa8ebd699eeca193c345c6a79637bbc
WattIQs <amaorbelo@gmail.com>
2026-08-30 02:03:17 -0300

    fix: use noExternals only on Render

--- vite.config.ts
+++ vite.config.ts
@@ -18,7 +18,7 @@ export default defineConfig({
     tanstackStart(),
     nitro({
       preset: isRender ? "node-server" : "vercel",
-      noExternals: true,
+      noExternals: isRender,
     }),
     viteReact(),
   ],
